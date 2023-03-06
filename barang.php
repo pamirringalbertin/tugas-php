23dc3e12562cf5a72c2b332021b6b53107a5bd3e
pamirringalbertin <119491618+pamirringalbertin@users.noreply.github.com>
2023-03-06 17:26:10 +0900

    Add files via upload

---
+++ barang.php
@@ -0,0 +1,24 @@
+<?php
+
+$harga= "D";
+
+switch ($harga) {
+    case "A";
+    echo "harga barang adalah 56.000" . PHP_EOL;
+    break;
+    case "B";
+    echo "harga barang adalah 80.000" . PHP_EOL;
+    break;
+    case "C";
+    echo "harga barang adalah 130.000" . PHP_EOL;
+    break;
+    case "D";
+    echo "harga barang adalah 15.000" . PHP_EOL;
+    break;
+    case "E";
+    echo "harga barang adalah 92.000" . PHP_EOL;
+    break;
+    default;
+    echo "Tidak ada harga dari barang tersebut" . PHP_EOL;
+    break;
+}
---
+++ bilangan.php
@@ -0,0 +1,7 @@
+<?php
+
+$nomor =3;
+
+$no = ($nomor % 2 == 0) ? "bilangan $nomor adalah bilangan genap" : "bilangan $nomor adalah bilangan ganjil";
+
+echo ($no);
